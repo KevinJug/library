8f9c08faa6038aeddc2de10d591f2c61422c35db
kevin.juguin <kevin.juguin@epsi.fr>
2020-11-20 15:25:16 +0100

    Mise à jour

--- backend/models/user_collection.model.js
+++ backend/models/user_collection.model.js
@@ -1,10 +1,6 @@
 module.exports = (sequelize, Sequelize) => {
 
     return sequelize.define('user_livre', {
-        tome : {
-            type: Sequelize.INTEGER,
-            allowNull: false
-        },
         description : {
             type: Sequelize.STRING(255),
             allowNull: true
